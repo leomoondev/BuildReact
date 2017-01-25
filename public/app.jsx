0dba178482a908ebaacd418d5de2046d6b84449f
Leo Moon <leomoondev@gmail.com>
2017-01-25 11:05:42 -0800

    Added component properties for message and name

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,27 @@
 var Greeter = React.createClass({
+  // Set a default values for name
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>This is from the component</p>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
       </div>
     );
   }
 });
 
+// Passing other types into props
+var firstName = 'Leo';
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
